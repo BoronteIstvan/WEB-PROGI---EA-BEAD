1336e0b0cecdce73c546b0d662e5d044e5aae96e
BoronteIstvan <boronte.isti03@gmail.com>
2026-04-18 18:12:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 var selectedIndex = null;
 var array1 = new Array();
-array1.push({"Frekvencia":"87,6	7,6","teljesitmeny":"Neo FM 	Győr","csatorna":"Szabadhegy","adohely":" "});
-// OR: array1[0]= {"Frekvencia":"John Smith","teljesitmeny":"data1@gmail.com","csatorna":"2000","adohely":"London"};
-array1.push({"Frekvencia":"87,6","teljesitmeny":"Kontakt Rádió","csatorna":"Budapest","adohely":"Terézváros "});
+array1.push({"Frekvencia":"87,6", "teljesitmeny":"7,6", "csatorna":"Neo FM", "adohely":"Győr"});
+array1.push({"Frekvencia":"87,6", "teljesitmeny":"0,5", "csatorna":"Kontakt Rádió", "adohely":"Budapest"});
+
 printArray();
+
 function printArray(){
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     table.innerHTML="";
-    var newRow;
-    for (i = 0; i < array1.length; i++) {
-        newRow = table.insertRow(table.length);
-        cell1 = newRow.insertCell(0);
-        cell1.innerHTML = array1[i].Frekvencia;
-        cell2 = newRow.insertCell(1);
-        cell2.innerHTML = array1[i].teljesitmeny;
-        cell3 = newRow.insertCell(2);
-        cell3.innerHTML = array1[i].csatorna;
-        cell4 = newRow.insertCell(3);
-        cell4.innerHTML = array1[i].adohely;
-        cell4 = newRow.insertCell(4);
-        cell4.innerHTML = '<a onClick="onEdit('+i+')">Edit</a>' + '<a onClick="onDelete('+i+')">Delete</a>';
+    for (var i = 0; i < array1.length; i++) {
+        var newRow = table.insertRow(table.length);
+        newRow.insertCell(0).innerHTML = array1[i].Frekvencia;
+        newRow.insertCell(1).innerHTML = array1[i].teljesitmeny;
+        newRow.insertCell(2).innerHTML = array1[i].csatorna;
+        newRow.insertCell(3).innerHTML = array1[i].adohely;
+        newRow.insertCell(4).innerHTML = '<a onClick="onEdit('+i+')">Szerkeszt</a> <a onClick="onDelete('+i+')">Töröl</a>';
     }
 }
+
 function onFormSubmit() {
     if (validate()) {
         var formData = readFormData();
-        if (selectedIndex==null)
+        if (selectedIndex == null)
             insertNewRecord(formData);
         else
             updateRecord(formData);
         resetForm();
     }
 }
+
 function readFormData() {
     var formData = {};
     formData["Frekvencia"] = document.getElementById("Frekvencia").value;
@@ -42,8 +39,7 @@ function readFormData() {
 }
 
 function insertNewRecord(data) {
-    array1.push({"Frekvencia":data.Frekvencia,"teljesitmeny":data.teljesitmeny,"csatorna":data.csatorna,"adohely":data.adohely});
-    // OR: array1[array1.length]= {"Frekvencia":data.Frekvencia,"teljesitmeny":data.teljesitmeny,"csatorna":data.csatorna,"adohely":data.adohely};
+    array1.push(data);
     printArray();
 }
 
@@ -52,38 +48,37 @@ function resetForm() {
     document.getElementById("teljesitmeny").value = "";
     document.getElementById("csatorna").value = "";
     document.getElementById("adohely").value = "";
-    selectedIndex=null;
+    selectedIndex = null;
 }
+
 function onEdit(index) {
     document.getElementById("Frekvencia").value = array1[index].Frekvencia;
     document.getElementById("teljesitmeny").value = array1[index].teljesitmeny;
     document.getElementById("csatorna").value = array1[index].csatorna;
     document.getElementById("adohely").value = array1[index].adohely;
-    selectedIndex=index;
+    selectedIndex = index;
 }
+
 function updateRecord(formData) {
-    array1[selectedIndex].Frekvencia=formData.Frekvencia;
-    array1[selectedIndex].teljesitmeny=formData.teljesitmeny;
-    array1[selectedIndex].csatorna=formData.csatorna;
-    array1[selectedIndex].adohely=formData.adohely;
+    array1[selectedIndex] = formData;
     printArray();
 }
+
 function onDelete(index) {
-    if (confirm('Are you sure to delete this record ?')) {
-        array1.splice(index, 1); // Deleting the entry with the specified index
+    if (confirm('Biztosan törölni akarod?')) {
+        array1.splice(index, 1);
         resetForm();
         printArray();
     }
 }
+
 function validate() {
-    isValid = true;
+    var isValid = true;
     if (document.getElementById("Frekvencia").value == "") {
         isValid = false;
         document.getElementById("FrekvenciaValidationError").classList.remove("hide");
     } else {
-        isValid = true;
-        if (!document.getElementById("FrekvenciaValidationError").classList.contains("hide"))
-            document.getElementById("FrekvenciaValidationError").classList.add("hide");
+        document.getElementById("FrekvenciaValidationError").classList.add("hide");
     }
     return isValid;
 }
